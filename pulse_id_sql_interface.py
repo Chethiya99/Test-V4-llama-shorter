411059a87da307bf36af3b640d9673e313800cbd
Jayan Nimna Chethiya <94435820+Chethiya99@users.noreply.github.com>
2025-01-31 09:05:40 +0530

    Update pulse_id_sql_interface.py

--- pulse_id_sql_interface.py
+++ pulse_id_sql_interface.py
@@ -291,7 +291,7 @@ if st.session_state.interaction_history:
                             task = Task(
                                 description=email_task_description.format(merchant_data=interaction['content']['extraction_results'].raw),
                                 agent=email_agent,
-                                expected_output="Marketing emails for each selected merchant, tailored to their business details. Please use a invisible line to seperate each emails if there is more than one"
+                                expected_output="Marketing emails for each selected merchant, tailored to their business details. Please use a line to seperate each emails if there is more than one"
                             )
 
                             # Crew execution 
